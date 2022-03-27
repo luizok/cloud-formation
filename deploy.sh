e688fd886c65388547fde52386a1534d966ce9c8
Luiz Felipe <luizfelipesmaciel@gmail.com>
2022-03-26 23:26:52 -0300

    Create a folder to save the files, if any

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ sed -E "s/AWS_ACCOUNT_ID/$AWS_ACCOUNT_ID/g" parameters.json > out/parameters.jso
 STACK_NAME=$(basename $(pwd))
 STACK_NAME=$(echo $STACK_NAME | sed -E 's/_/-/g')
 
-# echo $STACK_NAME
 cd parent_lambda/
 zip ../out/parent_function.zip -r .
 cd ..
@@ -15,14 +14,20 @@ cd ..
 cd child_lambda/
 zip ../out/child_function.zip -r .
 cd ..
-# read ans
 
+# Upload the source code to an s3 bucket
 aws cloudformation package \
     --template-file template.yml \
     --s3-bucket $S3_DEPLOY_BUCKET_NAME \
     --s3-prefix lambda-deploy-$STACK_NAME \
     --output-template-file out/template.yml
 
+# Create a folder to output files, if any
+aws s3api put-object \
+    --bucket $S3_OUTPUT_BUCKET_NAME \
+    --key lambda-deploy-$STACK_NAME-output/
+
+# Deploy the stack
 aws cloudformation deploy \
     --template-file out/template.yml \
     --stack-name $STACK_NAME \
