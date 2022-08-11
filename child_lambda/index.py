e24f36fee372efdf8c37a387d22067f679540612
Luiz Felipe <luizfelipesmaciel@gmail.com>
2022-08-11 20:30:24 -0300

    :sparkles: Added function to write into the bucket

--- child_lambda/index.py
+++ child_lambda/index.py
@@ -1,9 +1,30 @@
+import os
+import datetime as dt
+
+import boto3
+
+
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+
+def write_file(filename):
+
+    with open(f'/tmp/{filename}', 'w') as file:
+        file.write(filename[:-3])
+
+    s3_client = boto3.resource('s3')
+    bucket = s3_client.Bucket(BUCKET_NAME)
+
+    bucket.upload_file(f'/tmp/{filename}', filename)
+
+
 def handler(event, context):
+
+    timestamp = dt.datetime.now().timestamp()
+    filename = f'file_{timestamp}.txt'
+
+    write_file(filename)
     print('Child:', event)
-
-    event['childData'] = 'Im being called by my father'
-
-    print(event)
 
     return {
         'httpStatus': 200,
